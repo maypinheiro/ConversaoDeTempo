7821589d83abca052f36a1cc96de1fbaf4ca269b
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-22 12:33:13 -0300

    Exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConversaoDeTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+            TimeSpan segundos = TimeSpan.FromSeconds(entrada);
+            int horas =(((segundos.Days * 24) + segundos.Hours));
+
+            Console.WriteLine($"{horas.ToString("00")}:{segundos.Minutes.ToString("00")}:{segundos.Seconds.ToString("00")}");
+        }
+    }
+}
